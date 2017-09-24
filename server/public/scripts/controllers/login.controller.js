be465b677185d7720964cc24cd498dcd35c1fac7
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-09-23 23:44:30 -0500

    added more styling to be consistent throughout entire aoplication

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -11,7 +11,7 @@ medicalApp.controller('LoginController', function($http, $location, UserService)
       console.log('LoginController -- login');
       /* the code on line 13 checks for the presence of a username and password being enterd in the text fields on the DOM.  If nothing has been entered, a message is returned that says: Enter a username and password. */
       if(vm.user.username === '' || vm.user.password === '') {
-        vm.message = "Enter a username and password!";
+        vm.message = "Username and password can't be blank.";
       } else {/* if something IS entered in the username and password text fields, move on to make $http POST request */
         console.log('LoginController -- login -- sending to server...', vm.user);
         $http.post('/', vm.user).then(function(response) {/*making $http POST request to the database that will start a session on the user */ 
@@ -35,7 +35,7 @@ medicalApp.controller('LoginController', function($http, $location, UserService)
       console.log('LoginController -- registerUser');
       // below says we can't register a new user if the username field and the password field are blank
       if(vm.user.username === '' || vm.user.password === '') {/*runs if all username and password fields have not been properly filled in. This kicks off the $http POST request that will store the new user in the DB */
-        vm.message = "Choose a username and password!";
+        vm.message = "Username and password can't be blank.";
       } else { //if fields have been properly filled in
         console.log('LoginController -- registerUser -- sending to server...', vm.user); /* vm.user is the object that is created when the user registers and fills in the text boxes. This is on register.html as lc.user. */
         $http.post('/register', vm.user).then(function(response) {
